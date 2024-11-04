c46a39c947f1de5cf3d87c404640838140b2991b
LeeGaHyun12 <bearsi012@naver.com>
2024-11-04 10:09:40 +0900

    댓글

--- src/main/java/controller/board/BoardFormController.java
+++ src/main/java/controller/board/BoardFormController.java
@@ -6,10 +6,7 @@ import data.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +16,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @Controller
 @RequiredArgsConstructor
 public class BoardFormController {
